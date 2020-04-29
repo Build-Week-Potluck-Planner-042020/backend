194d678e56ffe4344ce74e9d6dd57a702756891a
Steven Wang <stevenwang2060@gmail.com>
2020-04-29 15:33:39 -0700

    Added more testing for foods router

--- auth/foods-router.spec.js
+++ auth/foods-router.spec.js
@@ -1,6 +1,8 @@
 const request = require("supertest");
 const server = require("../server.js");
 
+let foodID
+
 describe("POST to /api/foods", () => {
     it("add food", () => {
         return request(server)
@@ -10,9 +12,11 @@ describe("POST to /api/foods", () => {
             claimed: true
           })
           .then(res => {
+            foodID = res.body.id
             expect(res.status).toBe(201);
           })
     })
+
     it("returns JSON formatted body", () => {
         return request(server)
             .post("/api/foods")
@@ -20,4 +24,33 @@ describe("POST to /api/foods", () => {
                 expect(res.type).toMatch(/json/);
         });
     });
+
+    it("should get food data when handed a food ID", () => {
+        return request(server)
+          .get(`/api/foods/${foodID}`)
+          .then(res => {
+            expect(res.status).toBe(200)
+            expect(res.body.id).toBe(foodID)
+          })
+      })
+
+      it("should successfully edit a food", () => {
+        return request(server)
+          .put(`/api/foods/${foodID}`)
+          .send({
+            item_name: "new test food",
+            claimed: false
+          })
+          .then(res => {
+            expect(res.status).toBe(200)
+          })
+      })
+      
+      it("should delete food successfully", () => {
+        return request(server)
+          .delete(`/api/foods/${foodID}`)
+          .then(res => {
+            expect(res.status).toBe(200)
+          })
+      })
 })
